a5029dc3d5d1c2a0239d0dfa374fa9b283f24bd8
dantengsky <dantengsky@gmail.com>
2022-07-21 22:12:28 +0800

    exact storages-hive pipeline-sources catalogs-hive

--- common/pipeline-sources/src/sources/sync_source.rs
+++ common/pipeline-sources/src/sources/sync_source.rs
@@ -1,16 +1,16 @@
-// Copyright 2022 Datafuse Labs.
+//  Copyright 2022 Datafuse Labs.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 use std::any::Any;
 use std::sync::Arc;
@@ -19,12 +19,12 @@ use common_base::base::Progress;
 use common_base::base::ProgressValues;
 use common_datablocks::DataBlock;
 use common_exception::Result;
+use common_catalog::
 
-use crate::pipelines::processors::port::OutputPort;
-use crate::pipelines::processors::processor::Event;
-use crate::pipelines::processors::processor::ProcessorPtr;
-use crate::pipelines::processors::Processor;
-use crate::sessions::TableContext;
+use crate::processors::port::OutputPort;
+use crate::processors::processor::Event;
+use crate::processors::processor::ProcessorPtr;
+use crate::processors::Processor;
 
 /// Synchronized source. such as:
 ///     - Memory storage engine.
