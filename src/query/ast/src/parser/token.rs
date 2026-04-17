707019a6f8e32ad0170be31b1ff5f3d3bd9a2892
anonymous <anonymous>
2026-04-17 03:17:22 +0000

    fix(ast): unreserve PROPERTIES keyword to allow it as identifier
    PROPERTIES was added as a reserved keyword in #17812 for CREATE TABLE
    PROPERTIES syntax, but it does not need to be reserved. Reserving it
    prevents using `properties` as a bare column name in queries.

--- src/query/ast/src/parser/token.rs
+++ src/query/ast/src/parser/token.rs
@@ -1951,7 +1951,6 @@ impl TokenKind {
             | TokenKind::ORDER
             | TokenKind::OVER
             | TokenKind::PARTITION
-            | TokenKind::PROPERTIES
             | TokenKind::QUALIFY
             | TokenKind::ROWS
             | TokenKind::RANGE
