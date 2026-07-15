muzammilar/databend

fix(query): cancel open private task runs on drop (#20109)
* fix(query): cancel open private task runs on drop

* fix(query): bound dropped task cleanup by task id

* fix(query): clear stale task after edges on drop

* fix(query): run task drop cleanup outside warehouse filter

* fix(query): bound dropped run cleanup by monotonic task id

Drop the describe_task recheck and the name-scoped-state guards on the
DeleteTask path, restoring unconditional name-scoped cleanup of task
afters and the schedule key. Bound cancel_open_task_runs by
`task_id <= dropped_id` instead of an exact match: task ids are globally
monotonic, so any generation recreated after a drop has a strictly larger
id. This sweeps stale open EXECUTING runs (including deletes coalesced
away by a later same-name drop) without ever cancelling a run that
belongs to a newer, recreated task.

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

* fix(query): add private task cancel function

---------

Co-authored-by: Claude Opus 4.8 <noreply@anthropic.com>

src/meta/proto-conv/tests/it/main.rs
@@ -170,3 +170,4 @@ mod v175_field_stats_truncate_len;
 mod v176_task_script_sql;
 mod v177_arrow_file_format_params;
 mod v178_storage_config;
+mod v179_task_delete_task_id;

dantengsky/fuse-query

(cherry picked from commit 52f97993d80096c08e9e332e03bf138d88fd0075)
(cherry picked from commit 0cc5849f8164e14d6de504db9b06e34279f39800)

src/meta/proto-conv/tests/it/main.rs
@@ -168,3 +168,4 @@ mod v173_csv_quote_style;
 mod v174_user_key_pair;
 mod v175_field_stats_truncate_len;
 mod v176_task_script_sql;
+mod v177_task_delete_task_id;