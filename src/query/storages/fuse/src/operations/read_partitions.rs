385a7d5fe112a4b85736922f477571a90c33b508
kould <kould2333@gmail.com>
2026-09-04 06:45:45 +0800

    feat(storage): add rolling hash for ngram indexes
    (cherry picked from commit dca4606cbee9933a80be10904a174555e7d643dc)

--- src/query/storages/fuse/src/operations/read_partitions.rs
+++ src/query/storages/fuse/src/operations/read_partitions.rs
@@ -58,6 +58,7 @@ use databend_storages_common_cache::CachedObject;
 use databend_storages_common_index::BloomIndex;
 use databend_storages_common_index::DEFAULT_NGRAM_FALSE_POSITIVE_RATE;
 use databend_storages_common_index::NgramArgs;
+use databend_storages_common_index::NgramHashAlgorithm;
 use databend_storages_common_pruner::BlockMetaIndex;
 use databend_storages_common_pruner::TopNPruner;
 use databend_storages_common_table_meta::meta::BlockMeta;
@@ -824,6 +825,10 @@ impl FuseTable {
                 None => DEFAULT_NGRAM_FALSE_POSITIVE_RATE,
                 Some(s) => s.parse::<f64>()?,
             };
+            let hash_algorithm = match index.options.get("hash_algorithm") {
+                None => NgramHashAlgorithm::City64V0,
+                Some(s) => NgramHashAlgorithm::parse(s)?,
+            };
 
             for column_id in &index.column_ids {
                 let Some((pos, field)) = table_schema
@@ -839,6 +844,7 @@ impl FuseTable {
                     gram_size,
                     bloom_size,
                     false_positive_rate,
+                    hash_algorithm,
                 ));
             }
         }
